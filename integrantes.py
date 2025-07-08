d06fdfcfcf42e8056e249ae5d27f429e2864ea4e
Chupete23 <agu.nunez@duocuc.cl>
2025-07-07 22:56:26 -0400

    Agregado script de integrantes y clasificación de VLAN

---
+++ integrantes.py
@@ -0,0 +1,6 @@
+# integrantes.py
+
+integrantes = ["Agustín Nunez"]
+print("Integrantes del grupo:")
+for nombre in integrantes:
+    print("-", nombre)
---
+++ vlanchecker.py
@@ -0,0 +1,10 @@
+# vlan_checker.py
+
+vlan = int(input("Ingrese el número de VLAN: "))
+
+if 1 <= vlan <= 1005:
+    print("VLAN del rango normal")
+elif 1006 <= vlan <= 4094:
+    print("VLAN del rango extendido")
+else:
+    print("Número de VLAN inválido")
